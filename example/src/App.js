8a72079cb5db5b00996000ff4775d5fc41ebc8c4
Myckhel <myckhel1@hotmail.com>
2020-11-20 01:15:38 +0100

    basic `useComponent`

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
-import { ExampleComponent } from 'react-use-component'
-import 'react-use-component/dist/index.css'
+import { useComponent } from 'react-use-component'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+const App = (props) => {
+  const self = useComponent({
+    props,
+    state: {
+      abc: 'def',
+    },
+    getState: () => {
+      console.log(self.state)
+      return self.state
+    },
+    updateMe: (update) => {
+      self.me = update || 'Mike'
+      self.getState()
+      console.log('self', self);
+    }
+  })
+
+  useEffect(() => {
+    setTimeout(() => self.setState((s) => ({...s, value: 2})), 2000)
+    setTimeout(() => self.getState(), 4000)
+    setTimeout(() => self.updateMe(), 6000)
+    setTimeout(() => self.updateMe('Johnny'), 8000)
+  }, [])
+
+  console.log(self);
+  return <div>Create React Library Example 😄</div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, {useState, useRef} from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const merge = (a, b) => Object.assign({}, a, b)
+
+export const useMergeState = (initialState) => {
+  const [state, setState] = useState(initialState);
+  const ref = useRef(state)
+  ref.current = state
+
+  const setMergedState = (newState, callback) => {
+    setState((prevState) =>
+    typeof newState === 'function'
+      ? newState(ref.current)
+      : merge(prevState, newState)
+    );
+    callback && callback(state);
+  };
+
+  return [state, setMergedState, ref];
+};
+
+export const useComponent = (props) => {
+  const {state, _props = {}, ...rest} = props
+  const ref = useRef(rest)
+  const [_state, setState] = useMergeState(state)
+  ref.current.state = _state
+  ref.current.props = _props
+  ref.current.setState = setState
+
+  return ref.current
 }
